c2affe41122619c16401fe453d9886350bd881f6
Igor Kazakov <kiw@lefterova.ru>
2023-06-20 18:46:03 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// Метод вывода печати массива:
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+} 
