0ca7ef661f12a8814967fe6da8c1ff5e83477a3e
Igor Kazakov <kiw@lefterova.ru>
2023-06-20 18:52:02 +0500

    Выводим результат на экран

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿// Метод вывода печати массива:
+﻿// Создадим метод вывода печати массива:
 
 void PrintArray(string[] array)
 {
@@ -8,3 +8,36 @@ void PrintArray(string[] array)
     }
     Console.WriteLine();
 } 
+
+// Задаем начальный массив:
+
+Console.Write("Сколько элементов Вы хотите ввести? ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] arrayStrings = new string[size];
+for (int i = 0; i < size; i++)
+{
+    Console.WriteLine($"Введите {i+1}-й элемент: ");
+    string element = Convert.ToString(Console.ReadLine());
+    arrayStrings[i] = element;
+}
+
+// Проверяем элементы массива на выполнение условий задачи:
+
+string[] arrayFinal = new string[size];
+int len = 3;
+int pos = 0;
+
+for (int j = 0; j < size; j++)
+{
+    if (arrayStrings[j].Length <= len)
+    {
+        arrayFinal[pos] = arrayStrings[j];
+        pos++;
+    }
+}
+
+// Выводим результат на экран:
+
+Console.WriteLine();
+PrintArray(arrayFinal);
